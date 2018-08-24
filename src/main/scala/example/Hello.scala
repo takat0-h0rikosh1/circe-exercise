9f95dc65a50110bbff95095502e0b79ab40cf0b8
堀越貴斗 <t_horikoshi@septeni-original.co.jp>
2018-08-24 14:51:39 +0900

    decode処理もできるようになったぞ

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,19 +1,26 @@
 package example
 
-object Hello extends Greeting with App {
+import io.circe.{Decoder, DecodingFailure, HCursor, Json}
+
+object Hello extends App {
 
   import GenericLevelDerivation._
   import io.circe.syntax._
 
   val level1 = Level.L1
+
+  // encode 処理
   val encoded = level1.asInstanceOf[Level].asJson
-
   println(encoded)
-  println(greeting)
-}
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+  // decode 成功
+  val decodedRight = encoded.as[Level]
+  println(decodedRight)
+
+  // decode 失敗
+  val invalidJson = Json.obj(("level", Json.fromString("invalid json")))
+  val decodedLeft = invalidJson.as[Level]
+  println(decodedLeft)
 }
 
 sealed abstract class Level(val value: String)
@@ -22,6 +29,13 @@ object Level {
   case object L1 extends Level("level1")
   case object L2 extends Level("level2")
   case object L3 extends Level("level3")
+
+  def fromString(s: String): Option[Level] =
+    PartialFunction.condOpt(s)  {
+      case L1.value => L1
+      case L2.value => L2
+      case L3.value => L3
+    }
 }
 
 
@@ -30,9 +44,22 @@ object GenericLevelDerivation {
   import example.Level._
   import io.circe.{Encoder, Json}
 
+  val key = "level"
+
   implicit val encode: Encoder[Level] = Encoder.instance {
-    case l @ L1 => Json.fromString(l.value)
-    case l @ L2 => Json.fromString(l.value)
-    case l @ L3 => Json.fromString(l.value)
+    case l @ L1 => Json.obj((key, Json.fromString(l.value)))
+    case l @ L2 => Json.obj((key, Json.fromString(l.value)))
+    case l @ L3 => Json.obj((key, Json.fromString(l.value)))
+  }
+
+  implicit val decode: Decoder[Level] = new Decoder[Level] {
+    final def apply(c: HCursor): Decoder.Result[Level] =
+      for {
+        value <- c.downField(key).as[String]
+        maybeLevel = Level.fromString(value)
+        result <- maybeLevel
+          .map(Right(_))
+          .getOrElse(Left(DecodingFailure(s"invalid value: $value", c.downField(key).history)))
+      } yield result
   }
 }
