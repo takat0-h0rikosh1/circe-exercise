eafa3c86db16c74268a35e8cf6ea39448d30deff
takato.horikoshi <takato.h0rikosh1@gmail.com>
2018-08-31 23:18:00 +0900

    add TraversingAndModifying

---
+++ src/main/scala/example/TraversingAndModifying.scala
@@ -0,0 +1,46 @@
+package example
+
+import io.circe._
+import io.circe.parser._
+
+object TraversingAndModifying {
+
+  def main(array: Array[String]): Unit = {
+
+    val json =
+      """{
+        |  "id": "c730433b-082c-4984-9d66-855c243266f0",
+        |  "name": "Foo",
+        |  "counts": [1, 2, 3],
+        |  "values": {
+        |    "bar": true,
+        |    "baz": 100.001,
+        |    "qux": ["a", "b"]
+        |  }
+        |}""".stripMargin
+
+    val doc: Json = parse(json).getOrElse(Json.Null)
+    val cursor: HCursor = doc.hcursor
+
+
+    val baz: Decoder.Result[Double] =
+      cursor.downField("values").downField("baz").as[Double]
+    val baz2: Decoder.Result[Double] =
+      cursor.downField("values").get[Double]("baz")
+    val qux: Decoder.Result[Seq[String]] =
+      cursor.downField("values").downField("qux").as[Seq[String]]
+    val secondQux: Decoder.Result[String] =
+      cursor.downField("values").downField("qux")
+          .downArray.right.as[String]
+
+    val thirdQux: Decoder.Result[String] =
+      cursor.downField("values").downField("qux")
+        .downArray.deleteGoRight.as[String]
+
+    println(baz)
+    println(baz2)
+    println(qux)
+    println(secondQux)
+    println(thirdQux)
+  }
+}
