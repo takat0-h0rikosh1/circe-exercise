9c2d92c620bacf0cad47da7eaa4349af398d341f
takato.horikoshi <takato.h0rikosh1@gmail.com>
2018-09-02 17:09:05 +0900

    add SemiAutomaticDerivation

--- build.sbt
+++ build.sbt
@@ -2,6 +2,9 @@ import Dependencies._
 
 val circeVersion = "0.9.3"
 
+resolvers += Resolver.sonatypeRepo("releases")
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+
 lazy val root = (project in file(".")).settings(
   inThisBuild(
     List(
@@ -10,10 +13,10 @@ lazy val root = (project in file(".")).settings(
       version := "0.1.0-SNAPSHOT"
     )),
   name := "Hello",
-libraryDependencies
-  ++= Seq(
-    "io.circe" %% "circe-core",
-    "io.circe" %% "circe-generic",
-    "io.circe" %% "circe-parser"
-  ).map(_ % circeVersion)
+  libraryDependencies
+    ++= Seq(
+      "io.circe" %% "circe-core",
+      "io.circe" %% "circe-generic",
+      "io.circe" %% "circe-parser"
+    ).map(_ % circeVersion)
 )
---
+++ src/main/scala/example/SemiAutomaticDerivation.scala
@@ -0,0 +1,50 @@
+package example
+
+import example.SemiAutomaticDerivation.User
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto._
+import io.circe.syntax._
+import io.circe.generic.JsonCodec
+
+object SemiAutomaticDerivation {
+
+  case class Foo(a: Int, b: String, c: Boolean)
+  @JsonCodec case class Bar(i: Int, s: String)
+  case class User(id:  Long, firstName: String, lastName: String)
+
+  implicit val fooDecoder: Decoder[Foo] = deriveDecoder
+  implicit val fooEncoder: Encoder[Foo] = deriveEncoder
+
+  def main(array: Array[String]): Unit = {
+    val foo = Foo(1, "foo", true)
+    val json = foo.asJson
+    println(json)
+
+    val unit = json.as[Foo]
+    println(unit)
+
+    val json2 = Bar(13, "Qux").asJson
+    println(json2)
+
+    import UserCodec._
+
+    val user = User(123, "first name", "last name")
+
+    val json3 = user.asJson
+    val unit3 = json.as[User]
+
+    println(json3)
+    println(unit3)
+
+  }
+
+}
+
+object UserCodec {
+  implicit val decoderUser: Decoder[User] =
+    Decoder.forProduct3("id", "first_name", "last_name")(User)
+  implicit val encoderUser: Encoder[User] =
+    Encoder.forProduct3("id", "first_name", "last_name")(u =>
+      (u.id, u.firstName, u.lastName)
+    )
+}
