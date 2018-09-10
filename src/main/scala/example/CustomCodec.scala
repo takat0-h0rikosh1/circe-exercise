9204482520d0b905fddb0c9d66306fe1f87d944d
堀越貴斗 <t_horikoshi@septeni-original.co.jp>
2018-09-10 11:46:24 +0900

    add CustomeCodec

---
+++ src/main/scala/example/CustomCodec.scala
@@ -0,0 +1,37 @@
+package example
+
+import io.circe.Decoder.Result
+import io.circe.{Decoder, Encoder, HCursor, Json}
+
+object CustomCodec {
+
+  class Thing(val foo: String, val bar: Int)
+
+  implicit val encoder: Encoder[Thing] = new Encoder[Thing] {
+    final def apply(t: Thing): Json = Json.obj(
+      ("foo", Json.fromString(t.foo)),
+      ("bar", Json.fromInt(t.bar))
+    )
+  }
+
+  implicit val decoder: Decoder[Thing] = new Decoder[Thing] {
+    final def apply(c: HCursor): Result[Thing] =
+      for {
+        foo <- c.downField("foo").as[String]
+        bar <- c.downField("bar").as[Int]
+      } yield new Thing(foo, bar)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val thing = new Thing("test", 123)
+
+    import io.circe.parser._
+    import io.circe.syntax._
+
+    val jsonString = thing.asJson.noSpaces
+    println(parse(jsonString))
+    println(decode[Thing](jsonString))
+
+  }
+}
